54b79fed96da9f49468de3a6005655e38adcc8ac
Luiz <doginuwu@gmail.com>
2022-03-04 20:35:09 -0300

    🐛 fix: change theme hex

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,14 +5,14 @@ export const defaultTheme: DefaultTheme = {
     primary: {
       orange: "#FF5100",
       white: "#FFFFFF",
-      gray: "##C6C6C6",
+      gray: "#C6C6C6",
     },
     semantic: {
-      green: "##04D361",
+      green: "#04D361",
     },
     gray: {
       "300": "#BFBFBF",
-      "400": "##959595",
+      "400": "#959595",
     },
     background: "#282828",
   },
