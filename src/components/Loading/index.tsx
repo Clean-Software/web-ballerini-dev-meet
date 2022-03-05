c178378251a7955057cd35c955f402e23a939e0e
Luiz <doginuwu@gmail.com>
2022-03-04 22:50:25 -0300

    ✨ feat: add pattern svg and loading animation

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,7 @@
 import logoSVG from "@assets/LOGO.svg";
+import patternSVG from "@assets/pattern.svg";
 import Image from "next/image";
-import { Container } from "./styles";
+import { Container, Pattern } from "./styles";
 
 interface LoadingProps {
   isLoading?: boolean;
@@ -11,7 +12,11 @@ const Loading = ({ isLoading, children }: LoadingProps) => {
   if (isLoading)
     return (
       <Container>
+        <Pattern initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.5, repeat: Infinity }}>
+          <Image src={patternSVG} />
+        </Pattern>
         <Image src={logoSVG} width={300} height={350} />
+        <h1>Carregando...</h1>
       </Container>
     );
 
